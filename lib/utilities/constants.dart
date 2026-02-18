531eb91b9eab41d0b02a7f4f0daa7a3119743cc5
bakebrlk <bakebrlk@gmail.com>
2026-02-18 15:04:21 +0500

    remove constants.dart from tracking

--- lib/utilities/constants.dart
+++
@@ -1,8 +0,0 @@
-class Constants {
-  static const String WEATHER_APP_ID = "";
-  static const String WEATHER_BASE_SCHEME = "";
-  static const String WEATHER_BASE_URL_DOMAIN = "";
-  static const String WEATHER_FORECAST_PATH = "";
-  static const String IMAGES_PATH = "";
-  static const String WEATHER_IMAGES_URL = WEATHER_BASE_SCHEME + WEATHER_BASE_URL_DOMAIN + IMAGES_PATH;
-}
